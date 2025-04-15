fc811384ad3c9b0b512b92ba1ac242bb1388274f
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-14 21:41:44 -0300

    cli - collecting data for initial model

--- src/autopiper/cli.py
+++ src/autopiper/cli.py
@@ -1,8 +1,7 @@
 import argparse
-from autopiper.logger import setup_logger
+from autopiper import logger
 from autopiper.controllers import download_piper_package
-
-logger = setup_logger()
+from autopiper.csv_rows_to_modelname_dict import csv_rows_to_modelname_dict
 
 def main():
     parser = argparse.ArgumentParser(description="cli manager for c++ piper project")
@@ -18,6 +17,24 @@ def main():
         default=None,
         help="Tag name of the release to download (default: latest)",
     )
+    init_parser.add_argument(
+        "--lang-code",
+        type=str,
+        default="en_US",
+        help="Language code to use (default: en_US)",
+    )
+    init_parser.add_argument(
+        "--voice-model-name",
+        type=str,
+        default="amy",
+        help="Voice name to use (default: amy)",
+    )
+    init_parser.add_argument(
+        "--quality",
+        type=str,
+        default="low",
+        help="Quality of the voice model (default: low)",
+    )
     init_parser.set_defaults(func=init_command)
 
     args = parser.parse_args()
@@ -29,6 +46,11 @@ def init_command(args: argparse.Namespace) -> None:
     tag_name: str | None = args.tag_name
     logger.info("Project initialized with default settings.")
     download_piper_package(tag_name)
+    logger.info("Piper package downloaded successfully.")
+    key = f"{args.lang_code}.{args.voice_model_name}.{args.quality}"
+    d = csv_rows_to_modelname_dict()[key]
+    logger.info(f"Model link: {d['OnnxModelLink']}")
+    logger.info(f"Model config link: {d['OnnxModelConfigLink']}")
 
 
 if __name__ == "__main__":
