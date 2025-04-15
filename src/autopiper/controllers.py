a38cd6603e8c0525a4bcebfb629f425a3ec8e5e1
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-15 13:23:52 -0300

    refactor: update download_piper_package to use settings for output paths and improve code clarity

--- src/autopiper/controllers.py
+++ src/autopiper/controllers.py
@@ -1,9 +1,9 @@
 import os
 import tarfile
-from autopiper.logger import setup_logger
+from autopiper import logger
 from autopiper.download_github_release_asset import download_github_release_asset
+from autopiper import settings
 
-logger = setup_logger()
 
 def download_piper_package(tag_name: str | None) -> None:
     # https://github.com/rhasspy/piper
@@ -13,12 +13,12 @@ def download_piper_package(tag_name: str | None) -> None:
         repo="piper",
         tag_name=tag_name,
         asset_name="piper_linux_x86_64.tar.gz",
-        output_path="~/.cache/autopiper",
+        output_path=settings.CACHE_DIR,
     )
-    tar_gz_extract_dir = os.path.expanduser(f"~/.local/autopiper/{tag_name}")
+    tar_gz_extract_dir = settings.APP_ASSET_DIR.joinpath(tag_name)
     file = tarfile.open(tar_gz_path)
     file.extractall(path=tar_gz_extract_dir)
     file.close()
     logger.debug(f"Extracted {tar_gz_path} to {tar_gz_extract_dir}")
 
-    bin_dir = os.path.join(tar_gz_extract_dir, "piper")  # TODO save this to a config file
+    # bin_dir = os.path.join(tar_gz_extract_dir, "piper")  # TODO save this to a config file
