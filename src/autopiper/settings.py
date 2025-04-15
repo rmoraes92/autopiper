509045367818e166235ad05390dee93745aafbea
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-15 13:55:03 -0300

    refactor: add TODO comment for potential configuration file integration

--- src/autopiper/settings.py
+++ src/autopiper/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+# TODO maybe try read some of this from a json (or config) file?
 
 APP_ASSET_DIR: Path = Path(os.path.expanduser("~/.local/share/autopiper"))
 APP_ASSET_MODEL_DIR: Path = APP_ASSET_DIR.joinpath("models")
